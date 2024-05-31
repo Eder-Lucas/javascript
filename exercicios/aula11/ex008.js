b5e549c247a4b4c16190a9c83f5221e71ea8d60b
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-05-31 12:34:47 -0300

    fiz alguns exercicios e um desafio

---
+++ exercicios/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 12000
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) {// se for maior que 60km/h
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+
+console.log('dirija sempre usando cinto de segurança!')
+// condição simples
---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,8 @@
+var país = 'França'
+console.log(`Vivendo em ${país}`)
+
+if (país != 'Brasil') {// se não for diferente de Brasil
+    console.log('Você é brasileiro')
+} else {
+    console.log('Você é estrangeiro!')
+}
