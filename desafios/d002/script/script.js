b8b32d42e6daaac9cb5c3f5730419fe9dee3d3e1
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-07-09 17:14:54 -0300

    fiz o desafio 2

---
+++ desafios/d002/script/script.js
@@ -0,0 +1,36 @@
+var lamp = window.document.querySelector('#lampada')//seleciona a imagem
+var msg = window.document.querySelector('#mensagem')//seleciona o parágrafo
+
+//eventos
+lamp.addEventListener('mouseenter', entrar)
+lamp.addEventListener('mouseout', sair)
+lamp.addEventListener('click', clicar)
+
+var clicou = false//usuário ainda não clicou
+var clicks = 0//usuário ainda não deu nenhum click na imagem
+
+function clicar() {//quando clicar
+    clicou = true//usuário clicou
+    quantidade_clicks = clicks += 1//conta os clicks
+    lamp.src = "./imagens/lampada-quebrada.png"//troca a imagem
+    msg.innerHTML = "Ops! <strong>Recarregue a página</strong> e tenha mais cuidado!"//texto indicativo
+    console.log(`A lâmpada foi clicada e quebrou ${quantidade_clicks} vezes`)
+
+    if (clicks >= 2) {//apartir de 2 clicks essa mensagem se repete
+        alert('Recarregue a página para continuar a experiência')
+    }       
+}
+
+function entrar() {//quando entra
+    if (clicou == false) {//só executa quando a lâmpada NÃO está quebrada/clicada
+        lamp.src = "./imagens/lampada-ligada.png"//troca a imagem
+        console.log('A lâmpada ligou')
+    }
+}
+
+function sair() {//quando sai
+    if (!clicou) {//o operador "!" nega assim como a comparação anterior
+        lamp.src = "./imagens/lampada-desligada.png"//troca a imagem
+        console.log('A lâmpada desligou')
+    }
+}
