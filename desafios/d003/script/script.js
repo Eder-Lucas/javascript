509751b76457a222d990c4e89faaca71947913ed
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-07-10 23:59:23 -0300

    pré carregamento das imagens no desafio 3
    - agora as imagens estão sendo pré carregadas para evitar erros

--- desafios/d003/script/script.js
+++ desafios/d003/script/script.js
@@ -7,8 +7,7 @@ var msg = window.document.querySelector('p#mensagem')//texto básico
 //os dois observam quando a internet cair/offline ou voltar/online
 window.addEventListener('offline', verificaNet)
 window.addEventListener('online', verificaNet)
-
-img.addEventListener('click', clicar)//evento click
+img.addEventListener('click', clicar)//evento extra de click
 
 //quando clicar na imagem verifica se o navegador está com internet e executa as ações if e else
 function clicar() {
@@ -16,40 +15,51 @@ function clicar() {
         alert('Eu sou a internet! Estou bem e funcionando a todo vapor!')
     } else {// se não estiver = false
         alert('Não me sinto bem...Há algo errado comigo...')
-    }
+    }}
+    
+//pré carregando as imagens para evitar erros durante a operação
+//pré carregamento da imagem com sinal de internet desligado
+var imagemOff = new Image()//cria um objeto imagem
+imagemOff.onload = function () {//verifica se carregou
+    console.log('imagem offline carregada')
 }
+imagemOff.onerror = function () {//verifica se não carregou
+    console.log('imagem offline não foi carregada')
+}
+imagemOff.src = './imagens/sem-wifi.png'//atribui o caminho da imagem com sinal de internet desligado
+imagemOff.alt = 'Sinal-wifi-desligado'//atribui o texto alternativo
+
+//pré carregamento da imagem com sinal de internet ligado
+var imagemOn = new Image()
+imagemOn.onload = function () {
+    console.log('imagem online carregada')
+}
+imagemOn.onerror = function () {
+    console.log('imagem online não foi carregada')
+}
+imagemOn.src = './imagens/wifi.png'
+imagemOn.alt = 'Sinal-wifi-ligado'
 
 //função que analisa a conexão com internet
 function verificaNet() {
-    img.style.width = '200px'//a imagem wifi diferente da carregada no html serão menores
-
     if (navigator.onLine) {//se estiver online
-        //coloca a imagem e o alt
-        img.src = "./imagens/wifi.png"
-        img.alt = 'Sinal-wifi-ligado'//caso a imagem não carregue
-
-        //interação com o html
+        console.log('Usuário online')//avisa que o usuário está com internet
+        img.src = imagemOn.src//carregamento das imagens e o texto alternativo
+        img.alt = imagemOn.alt
         statusNet.innerHTML = ' Online'//coloca o status como online
         msg.innerHTML = 'Você tem internet! O que você mais gosta de fazer com ela?'
-        statusNet.style.color = 'lime'//coloca a cor como verde para simbolizar que está tudo bem
+        statusNet.style.color = 'lime'//coloca a cor como verde para simbolizar que está online
         msg.style.color = 'lime'
-        
-        console.log('Usuário online')
     } 
     else {//se estiver offline
-        //troca a imagem e o alt
-        img.src = './imagens/sem-wifi.png'
-        img.alt = 'Sinal-wifi-desligado'//caso a imagem não carregue( muito provavelmente não irá )
-
-        //interação com o html        
+        console.log('Usuário offline')//avisa que o usuário está sem internet
+        img.src = imagemOff.src//carregamento das imagens e o texto alternativo
+        img.alt = imagemOff.alt        
         statusNet.innerHTML = ' Offline'//troca o status por offline
         msg.innerHTML = 'Nem dados movéis? Que tal jogar o Dinossauro do Chrome?'
-        statusNet.style.color = 'red'//fica com cor vermelha para simbolizar a queda
+        statusNet.style.color = 'red'//fica com cor vermelha para simbolizar que está offline
         msg.style.color = 'red'
-
-        console.log('Usuário offline')
-    }
-}
+    }}
 
 //chama a função para verificar dinamicamente se está com internet ou não 
 verificaNet()
