32d02883551fd84313c84c0428464d4b71f4317d
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-07-18 16:48:42 -0300

    melhoria no JavaScript do desafio 1
    - um evento clique mais compatível com dispositivos móveis
    - uma estrutura melhor de código

--- desafios/d001/script/script.js
+++ desafios/d001/script/script.js
@@ -3,27 +3,21 @@ window.alert('Você está prestes a desembarcar no aeroporto quando é parado po
 //eventos
 var enviar = window.document.getElementById('enviar')
 enviar.addEventListener('click', nacionalidade)//quando clica em enviar
-enviar.addEventListener('mouseenter', entrar)//quando mouse entra 
-enviar.addEventListener('mouseout', saida)//quando o mouse sai
-
-function entrar() {//quando o mouse entrar no botão enviar
-    enviar.style.background = 'gray'
-}
-
-function saida() {//quando o mouse sair do botão enviar
-    enviar.style.background = 'white'
-}
 
 function nacionalidade() {
-    enviar.style.background = 'gray'
     var btnPais = window.document.getElementById('pais')
     var resposta = window.document.getElementById('res')
     var pais = btnPais.value//pega o valor definido no HTML
 
     if (pais != 'Brasil') {//se não for do Brasil
         resposta.innerHTML = `<p> <strong>MENTIRA!</strong> Você não veio do <strong>${pais}</strong> </p>`
-        window.alert('VOCÊ ESTÁ PRESO!')
     } else {//se for do Brasil
         resposta.innerHTML = `<p> <strong>CORRETO!</strong> Você veio do <strong>${pais}</strong>, pode passar.</p>`
     }
+    
+    enviar.style.background = 'gray'//quando clicar o fundo do botão enviar fica cinza
+    setTimeout( () => {//após 80 milissegundos ele volta a ter o fundo branco
+        enviar.style.background = ''
+    }, 80)
+
 }
