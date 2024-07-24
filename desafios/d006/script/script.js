fa19e279f12c89fd44ec1910d4e1656aa49d22cb
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-07-23 23:04:52 -0300

    hora automática no desafio 6
    - com o setInterval a função que muda a hora é ativada a cada 1 segundo

--- desafios/d006/script/script.js
+++ desafios/d006/script/script.js
@@ -2,18 +2,17 @@ var msgStats = document.querySelector('#saudacao')
 var parágrafo = document.querySelector('#msg')
 var img = document.querySelector('#img')
 
+function atualizaHora() {
+    var atual = new Date()
+    var hora = atual.getHours()
+    var corpo = document.body
 
-function atualizaParágrafo(hora) {
     if (hora === 1 || hora === 0) {
         parágrafo.innerHTML = `Agora é: <strong>${hora}h</strong>`
     }
     else {
         parágrafo.innerHTML = `Agora são: <strong>${hora}h</strong>`
     }
-}
-
-function atualizaImagem(hora) {
-    var corpo = document.body
 
     if (hora >= 6 && hora < 12) {
         img.src = 'imagens/foto-manha.png'
@@ -27,39 +26,42 @@ function atualizaImagem(hora) {
         img.alt = 'foto representando a manhã'
         corpo.style.color = 'black'
         corpo.style.background = '#f8b058'
-        msgStats.textContent = 'meio-dia! Já almoçou?'       
+        msgStats.textContent = 'meio-dia! Já almoçou?'
     }
     else if (hora > 12 && hora < 18) {
         img.src = 'imagens/foto-tarde.png'
         img.alt = 'foto representando a tarde'
+        corpo.style.color = 'white'
         corpo.style.background = '#D26A73'
         msgStats.textContent = 'Boa tarde!'
     }
     else if (hora >= 18 && hora < 24) {
         img.src = 'imagens/foto-noite.png'
         img.alt = 'foto representando a noite'
+        corpo.style.color = 'white'
         corpo.style.background = '#020659'
         msgStats.textContent = 'Boa noite!'
     }
     else if (hora === 0) {
         img.src = 'imagens/foto-madrugada.png'
         img.alt = 'foto representando a madrugada'
+        corpo.style.color = 'white'
         corpo.style.background = 'black'
         msgStats.textContent = 'Meia-noite! O que faz acordado?'
     }
     else {
         img.src = 'imagens/foto-madrugada.png'
         img.alt = 'foto representando a madrugada'
+        corpo.style.color = 'white'
         corpo.style.background = 'black'
         msgStats.textContent = 'Está tudo tão quieto...'
     }
 }
 
 window.onload = () => {
-    var atual = new Date()
-    var hora = atual.getHours()
+    atualizaHora()
 
-    atualizaParágrafo(hora)
-    atualizaImagem(hora)
-    console.log('Funções carregadas. Programa executando corretamente')
+    setInterval(() => {
+        atualizaHora()
+    }, 1000);
 }
