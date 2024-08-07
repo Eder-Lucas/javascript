00c4764ff34dfd65778dca9fefd97f591a4c360a
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-08-07 20:45:01 -0300

    comentários no ex002 aula14

--- exercicios/aula14/ex002.js
+++ exercicios/aula14/ex002.js
@@ -1,14 +1,13 @@
 var sec = 5
-for (contador = 0; contador <= sec; contador++) {
+for (contador = 0; contador <= sec; contador++) {//faz uma ordem decrescente 5 até 0
     setTimeout(() => {
         var segundos = ''
-        function cont(sec, segundos) {
+        function cont(sec, segundos) {//retorna o texto já validado
             return `${sec} ${sec === 1 || sec === 0? segundos = 'segundo': segundos = 'segundos'}`
-
         }
         console.log(cont(sec, segundos)) 
         sec--
-    }, contador * 1000)
+    }, contador * 1000)//executa a cada segundo
 }
 
 
