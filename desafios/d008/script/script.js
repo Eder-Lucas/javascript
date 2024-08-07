b0d2e114a3b1235ebfe9592fb1990db0cb70e51c
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-08-07 20:50:29 -0300

    criei a base do desafio 8

---
+++ desafios/d008/script/script.js
@@ -0,0 +1,6 @@
+alert('Hello Word!')
+
+var contar = document.querySelector('#contar').addEventListener('click', () => {
+    var res = document.querySelector('#res')
+    res.textContent = 'interações em breve...'
+})
