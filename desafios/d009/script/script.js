9c6622df9503a191a633620ded66701ef7d23de0
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-10-19 15:30:49 -0300

    criei o arquivo js: d009

--- desafios/d009/script/script.js
+++ desafios/d009/script/script.js
@@ -1,1 +1,22 @@
-alert('Hello Word')
+var gerarTab = window.document.querySelector('input#gerar')
+gerarTab.addEventListener('click', tabuada)
+
+function tabuada() {
+    var num = window.document.querySelector('input#num')
+    var tab = window.document.querySelector('select#seltab')
+
+    if (num.value.length == 0) {
+        alert('Valor inválido')
+    } else {
+        var n = Number(num.value)
+        tab.innerHTML = ''
+
+        for (let i = 1; i <= 10; i++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${i} = ${n * i}`
+            item.value = `tab${i}`
+            tab.appendChild(item)
+        }
+    }
+
+}
