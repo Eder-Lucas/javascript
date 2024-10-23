f1482685a95c97653eb7f68fa33df96c70e046b1
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2024-10-22 22:25:23 -0300

    adicionei alguns exercícios na aula 15

---
+++ exercicios/aula15/ex001.js
@@ -0,0 +1,29 @@
+//variável composta
+
+let num = [3, 9, 5, 7]
+
+//coloca o valor 2 no final 
+num.push(2)
+
+//organiza os números em ordem crescente
+//considera apenas o primeiro número
+//podendo haver sitações onde o 10 venha primeiro que o 2
+num.sort()
+
+//mostra os valores em colchetes 
+console.log(num)
+
+//diz quantos elementos tem uma array
+console.log(`O vetor tem ${num.length} posições`)
+
+//mostrando o elemento de índice 0
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+//vê em que lugar está o valor 5 na array
+//quando o valor não é encontrado ele retorna -1
+let pos = num.indexOf(5)
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 5 está na posição ${pos}`)
+}
---
+++ exercicios/aula15/ex002.js
@@ -0,0 +1,17 @@
+let valores = [2, 5, 3, 9, 8]
+valores.sort()
+
+//mostra os valores de forma tradicional => []
+console.log(valores)
+/*
+uma forma de ver os valores visualmente com outra formatação
+
+for(let pos = 0; pos< valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
+*/
+
+//simplifica o código acima
+for(let pos in valores) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
+}
