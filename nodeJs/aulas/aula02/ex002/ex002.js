b3a7b4971eb3b0057c9db55985a3ef0eaee70b04
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-07-24 00:41:14 -0300

    feat: cria ex002 na aula02

---
+++ nodeJs/aulas/aula02/ex002/ex002.js
@@ -0,0 +1,34 @@
+import express from "express"
+const app = express()
+const PORT = 5000
+
+app.listen(PORT, () => {
+    console.log(`Servidor ex002 rodando na porta ${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send("Página inicial do servidor")
+})
+
+app.get('/sobre', (req, res) => {
+    res.send("barbearia do joaquim")
+})
+
+app.get('/servicos', (req, res) => {
+    res.json(servicos)
+})
+
+const servicos = [
+    {
+        id: 1, 
+        nome: "corte americano", 
+        preco: 20, 
+        tempo: "20min"
+    },
+        {
+        id: 2, 
+        nome: "degradê alto", 
+        preco: 25, 
+        tempo: "26min"
+    },
+]
