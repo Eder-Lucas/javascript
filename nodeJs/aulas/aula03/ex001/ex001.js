fa6f85596e4149abef236e8422fd2458e4b974ba
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-07-27 00:37:44 -0300

    feat: cria rota que pesquisa usuario por id

--- nodeJs/aulas/aula03/ex001/ex001.js
+++ nodeJs/aulas/aula03/ex001/ex001.js
@@ -10,6 +10,34 @@ app.get("/usuarios/:id/:nome", (req, res) => {
     res.send("OK")
 })
 
+// Pesquisa por um usuário existente usando o id
+app.get("/usuariosBuscar/:id", (req, res) => {
+    const id = Number(req.params.id) // Converte pois sempre vem como string
+
+    // Pega cada elemento da base de dados: "usuarios"
+    // Coloca na variavel u e percorre
+    // u = { id:2, nome "João" }, quando acha retorna o objeto
+    const usuario = usuarios.find(u => u.id == id)
+
+    // Quando não acha retorna Undefined, o js considera isso false
+    // Status define a resposta http
+    // Envia uma mensagem em json
+    if(!usuario) {
+        return res.status(404).json({
+            mensagem: "Usuário não encontrado"
+        })
+    }
+
+    res.send(usuario)
+})
+
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Servidor rodando na porta ${PORT}`)
 })
+
+// Base de dados simples
+const usuarios = [
+    { id: 1, nome: "Pedro" },
+    { id: 2, nome: "Maria" },
+    { id: 3, nome: "Bruno" }
+]
