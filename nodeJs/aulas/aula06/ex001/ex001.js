d3480146154dd620b00741f04b9bc947d48cdc48
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-08-13 22:49:45 -0300

    feat: cria os arquivos base da aula06

---
+++ nodeJs/aulas/aula06/ex001/ex001.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import chalk from 'chalk'
+import 'dotenv/config'
+import path from 'path'
+
+const app = express()
+const PORT = 5000
+const SERVER_IP = process.env.SERVER_IP
+
+app.use(express.json())
+app.use(express.static(path.join(import.meta.dirname,"public")))
+
+// Estilo do chalk
+const title = chalk.rgb(255, 23, 189)
+const link = chalk.rgb(255, 225, 0)
+
+app.get('/user', (req, res) => {
+    res.json({
+        mensagem: "Essa mensagem veio do servidor"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(title("Servidor: " + title.bgRgb(1,1,1)(" 06_EX001 ") + " rodando na porta " + title.bgRgb(1,1,1)(` ${PORT} `)))
+    console.log("Acesso local: " + link.underline((`http://localhost:${PORT}/`)))
+    console.log("Acesso por IP: " + link.underline((`http://${SERVER_IP}:${PORT}/`)))
+})
