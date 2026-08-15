de312385469608e5bbc2ab7ecb8707a6fa21a1d7
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-08-15 17:48:33 -0300

    feat: cria o server do ex002

---
+++ nodeJs/aulas/aula06/ex002/server/api.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cors from 'cors'
+import chalk from 'chalk'
+import 'dotenv/config'
+
+export function startServer() {   
+    const app = express()
+    const PORT = 4000
+    const SERVER_IP = process.env.SERVER_IP
+
+    // Nesse exercício, o front e back estão em origens diferentes
+    // Isso causa erro de CORS
+    app.use(express.json())
+    app.use(cors()) // Permite acessar requisições de qualquer origem
+
+    // Estilo do chalk
+    const title = chalk.rgb(255, 23, 189)
+    const link = chalk.rgb(255, 225, 0)
+
+    app.get('/', (req, res) => {
+        res.json({
+            mensagem: "Servidor pronto!"
+        })
+    })
+
+    app.get('/usuarios', (req, res) => {
+        res.json({
+            mensagem: "Acessando usuários"
+        })
+    })
+
+    app.listen(PORT, () => {
+        console.log(title("Servidor: " + title.bgRgb(1,1,1)(" 06_EX002 ") + " rodando na porta " + title.bgRgb(1,1,1)(` ${PORT} `)))
+        console.log("Acesso local: " + link.underline((`http://localhost:${PORT}/`)))
+        console.log("Acesso por IP: " + link.underline((`http://${SERVER_IP}:${PORT}/`)))
+    })
+}
