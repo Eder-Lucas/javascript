ce138a555e33fd4ebd32db0aaff0e33d1849fa35
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-08-15 16:50:27 -0300

    feat: mostra os usuários salvos no front-end

--- nodeJs/aulas/aula06/ex001/public/fetch.js
+++ nodeJs/aulas/aula06/ex001/public/fetch.js
@@ -1,12 +1,26 @@
 const p = document.querySelector("p#o")
 const btn = document.querySelector("button#a")
+const nome = document.querySelector("input#nome")
+const listUser = document.querySelector("p#lista")
 
 btn.addEventListener('click', lista)
 async function lista() {
     const resposta = await fetch("http://192.168.1.16:5000/user")
+    const salvaNome = await fetch("http://192.168.1.16:5000/salvar", {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome.value
+        })
+    })
 
+    const users = await salvaNome.json()
     const itens = await resposta.json()
+    const ultimo = users[users.length - 1]
 
-    p.innerText = "A API retornou: " + itens.mensagem
-    console.log(mensagem)
+    p.innerHTML = "A API retornou: " + itens.mensagem + "<br>"
+    p.innerHTML += "Novo usuário: " + JSON.stringify(ultimo)
+    listUser.innerText = JSON.stringify(users)  
 }
