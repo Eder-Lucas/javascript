93e408febaef28ff9020664e144607a7e2fee8c2
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-08-16 22:38:11 -0300

    feat: cria a função de salvar um usuário em memória

--- nodeJs/aulas/aula06/ex002/frontend/script.js
+++ nodeJs/aulas/aula06/ex002/frontend/script.js
@@ -1,11 +1,34 @@
 const listaResposta = document.querySelector('p#resposta')
+const btnEnviar = document.querySelector('input#enviar')
+const campoNome = document.querySelector('input#nome')
+const campoIdade = document.querySelector('input#idade')
 
 async function carregar() {
     const resposta = await fetch("http://192.168.1.16:4000/")
     const dados = await resposta.json()
 
-    console.log(dados)
-    listaResposta.innerText = JSON.stringify(dados)
+    listaResposta.innerText = dados.mensagem
 }
 
 carregar()
+
+btnEnviar.addEventListener('click', salvarUsuario)
+
+async function salvarUsuario() {
+    const resposta = await fetch("http://192.168.1.16:4000/usuarios", {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: campoNome.value,
+            idade: campoIdade.value
+        })
+    })
+
+    const usuario = await resposta.json()
+
+    listaUsuarios()
+
+    listaResposta.innerHTML = usuario.mensagem
+}
