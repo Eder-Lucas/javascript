1f81629e785a93814f901c4904781692c94197db
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-09-05 11:45:46 -0300

    feat: cria função no frontend para salvar um usuário e cria abort se o servidor demorar a responder

--- nodeJs/aulas/aula07/ex001/frontend/script.js
+++ nodeJs/aulas/aula07/ex001/frontend/script.js
@@ -1,16 +1,50 @@
 const mostraCod = document.querySelector('p#resposta')
 const btnCod = document.querySelector('button#cod')
+const nome = document.querySelector('input#nome')
+const btnEnviar = document.querySelector('input#enviar')
 
-btnCod.addEventListener('click', clique)
+btnCod.addEventListener('click', servidorAtivo)
 
-async function clique() {
+async function servidorAtivo() {
     try {
-        const resposta = await fetch('http://192.168.1.17:5050/')
+        const controlador = new AbortController()
+
+        setTimeout(() => controlador.abort(), 2000)
+
+        const resposta = await fetch(
+            'http://192.168.1.15:5050/',
+            {
+                signal: controlador.abort()
+            }
+        )
         const processo = await resposta.json()
 
+        mostraCod.textContent = ""
         mostraCod.textContent = processo.mensagem
+
+        return true
     } catch (error) { // Se não consegui realizar o fetch o servidor está inativo
-        mostraCod.textContent = "Servidor offline"
+        mostraCod.textContent = "Servidor está inativo"
+
+        return false
     }
-    
+}
+
+btnEnviar.addEventListener('click', SalvarUsuario)
+
+async function SalvarUsuario() {
+    const resposta = await fetch('http://192.168.1.15:5050/usuarios', {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome.value,
+        })
+    })
+
+    const usuarios =  await resposta.json()
+
+    mostraCod.textContent = ""
+    mostraCod.textContent = usuarios.mensagem
 }
