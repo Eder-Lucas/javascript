c0551bdac172ed46a7370455c0423de0303f060c
Eder Lucas <157969653+Eder-Lucas@users.noreply.github.com>
2026-09-11 22:14:42 -0300

    feat: adiciona função para monitorar o status do servidor

--- nodeJs/aulas/aula08/ex001/frontend/index.js
+++ nodeJs/aulas/aula08/ex001/frontend/index.js
@@ -2,6 +2,7 @@ const btnEnviar = document.querySelector('input#enviar')
 const campoNome = document.querySelector('input#nome')
 const campoIdade = document.querySelector('input#idade')
 const res = document.querySelector('p#resposta')
+const res_server = document.querySelector('p#resposta-server')
 
 const URL_API = 'http://192.168.1.15:4040'
 
@@ -29,5 +30,34 @@ async function SalvarUsuario() {
     } catch (erro) {
         console.log('Erro: ', erro)
     }
-
 }
+
+async function Status() {
+    try {
+        const controlador = new AbortController()
+
+        setTimeout(()=> controlador.abort(), 2000)
+
+        const resposta = await fetch(
+            `${URL_API}/status`,
+            {
+                signal: controlador.signal
+            } 
+        )
+
+        if (resposta.ok) {
+            const dados = await resposta.json()
+            res_server.textContent = dados.status
+        }
+    } catch (erro) {
+        if (erro.name === "AbortError") {
+            res_server.textContent = "Servidor demorou demais"
+        } else {
+            res_server.textContent = "Não foi possivel conectar ao servidor"
+        }
+    }  
+}
+
+setInterval(() => {
+    Status()
+}, 2000)
